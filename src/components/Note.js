7517bc225c730d88503ea08313710e76c10541f6
1337-hacks <65492624+1337-hacks@users.noreply.github.com>
2022-08-25 19:11:53 +1200

    FM1 more note functionality added

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,15 +1,33 @@
 import Card from 'react-bootstrap/Card';
 import EditIcon from '@mui/icons-material/Edit';
+import React, {useState} from "react";
+import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
+import Draggable from "react-draggable";
 
 function Note(props) {
+
+  const [hover, setHover] = React.useState(false);
+
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>{props.heading}</Card.Title>
-        <Card.Text>{props.text}</Card.Text>
-        <Card.Link href="#"><EditIcon/></Card.Link>
-      </Card.Body>
-    </Card>
+    <Draggable>
+      <Card style={{ width: '18rem' }} className="note" onMouseOver={() =>setHover(true)} onMouseLeave={() =>setHover(false)}>
+        <Card.Body>
+          <Card.Title>{props.heading}</Card.Title>
+          <Card.Text>{props.text}</Card.Text>
+        </Card.Body>
+        {
+          hover && 
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>
+                <Button variant="link"><EditIcon/></Button>
+                <Button variant="link" onClick={() => props.onClick(props.id)}><DeleteRoundedIcon/></Button>
+              </ListGroup.Item>
+            </ListGroup>
+        }
+      </Card>
+    </Draggable>
   );
 }
 
