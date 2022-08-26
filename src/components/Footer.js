c78fe6f2550faa97f738d11c7b1b36abae447535
1337-hacks <65492624+1337-hacks@users.noreply.github.com>
2022-08-26 22:46:46 +1200

    FM2 note styling and components added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
 function Footer() {
     const year = new Date().getFullYear();
     return (
-        <footer>
-        <p>Copyright ⓒ {year}</p>
-        </footer>
+        <Container>
+            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <p>Copyright ⓒ {year}</p>
+            </footer>
+        </Container>
     );
 }
 
