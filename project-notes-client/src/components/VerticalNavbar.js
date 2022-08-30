58dc5fdae3a66da91c3477adf1e9964a54ad1a20
1337-hacks <65492624+1337-hacks@users.noreply.github.com>
2022-08-30 19:26:45 +1200

    FM4 initialize backend for merging to main

--- project-notes-client/src/components/VerticalNavbar.js
+++ project-notes-client/src/components/VerticalNavbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import MenuIcon from '@mui/icons-material/Menu';
--- src/components/Workspace.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Workspace() {
-    return (<div></div>);
-}
-
-export default Workspace;
